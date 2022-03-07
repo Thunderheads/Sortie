5027c5b52f2e5cde960e6d4679fbcf4805745080
Najim AMOKHTARI <najim.amokhtari2021@campus-eni.fr>
2022-03-07 16:53:57 +0100

    jour 1 creation repo vide

---
+++ src/Entity/Campus.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CampusRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CampusRepository::class)
+ */
+class Campus
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
