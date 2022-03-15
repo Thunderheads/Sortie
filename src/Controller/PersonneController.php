ab90fe38256622b9e55c334de6f51aad6a04152a
xMarc007x <marc.mainguy@laposte.net>
2022-03-15 09:01:36 +0100

    commit profil ending

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -3,14 +3,17 @@
 namespace App\Controller;
 
 use App\Entity\Participant;
-use App\Form\SortieType;
 use App\Form\UserType;
 use App\Repository\ParticipantRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class PersonneController extends AbstractController
 {
@@ -18,34 +21,49 @@ class PersonneController extends AbstractController
     /**
      * @Route("/profil/{id}", name="monProfil")
      */
-    public function profil(Participant $particpant, ParticipantRepository $partRepo, Request $req, SluggerInterface $slugger): Response
+    public function profil(Participant $participant, ParticipantRepository $partRepo, Request $req, SluggerInterface $slugger, EntityManagerInterface $em, UserPasswordHasherInterface $passHach): Response
     {
         //je crée le formulaire
-        $formUser = $this->createForm(UserType::class, $particpant);
+
+        $formUser = $this->createForm(UserType::class, $participant);
         $formUser->handleRequest($req);
         if ($formUser->isSubmitted() && $formUser->isValid()) {
+
             $imageDoss = $formUser->get('image')->getData();
-//                $hashPassword = $formUser->get($particpant)->getPassword();
-//
-//                $particpant->setPassword($hashPassword);
+
+                $hashPassword = $formUser->get('password')->getData();
+                $participant->setPassword($hashPassword);
+                $encodePass = $passHach->hashPassword(
+                    $participant,
+                    $hashPassword
+                );
+                $participant->setPassword($encodePass);
+                $em->persist($participant);
+                $em->flush();
+
+                dump('profilModifier');
+
             if ($imageDoss) {
                 $nomFichier = pathinfo($imageDoss->getClientOriginalName(), PATHINFO_FILENAME);
                 $safeNom = $slugger->slug($nomFichier);
                 $newNom = $safeNom . '-' . uniqid() . '.' . $imageDoss->guessExtension();
+
                 try {
                     $imageDoss->move(
-                        $this->getParameter('img_drectory'),
+                        $this->getParameter('img_directory'),
                         $newNom
                     );
                 } catch (FileException $e) {
                 }
-                $particpant->setImage($newNom);
+                $participant->setImage($newNom);
+                $em->persist($participant);
+               $em->flush();
             }
-            return $this->redirectToRoute('home');
+
+//            return $this->render('sortie/monProfil.html.twig');
+
         }
-//            $em = $this->getUser();
-//            $em->persist($user);
-//            $em->flush();
+
         return $this->render('sortie/monProfil.html.twig', [
             'formUser' => $formUser->createView()
         ]);
